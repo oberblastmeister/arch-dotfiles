184d724b0d1186e57a60a5ee4f3e328b6fb97c02
Brian Shu <littlebubu.shu@gmail.com>
2020-07-03 11:13:51 -0400

    Mimeopen alias

---
+++ bin/lf-launch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Launch-: The XDG Killer with ing feature
+# Launches files based on their mimetype
+# Usage: launch- [FILE...]
+
+case $(file --mime-type "$*" -bL) in
+    # Check for the mimetype of your file
+    text/* | inode/x-empty | application/json | application/octet-stream)
+        # Launch using your favorite application
+        "$EDITOR" "$*"
+        ;;
+    video/* | audio/* | image/gif)
+        mpv "$*" &
+        ;;
+    application/pdf | application/postscript)
+        # zathura throws some warnings for some reasons
+        zathura "$*" &>/dev/null 2>&1 &
+        ;;
+    image/*)
+        sxiv "$*" &
+        ;;
+    application/zip)
+        unzip "$*" -d "${1%.*}"
+        ;;
+esac
---
+++ bin/nmgui
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+nm-applet    2>&1 > /dev/null &
+stalonetray  2>&1 > /dev/null
+killall nm-applet

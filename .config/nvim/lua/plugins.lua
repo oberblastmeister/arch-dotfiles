b1babc9c105b744eb1e872faa25e9876080115f7
Brian Shu <littlebubu.shu@gmail.com>
2020-10-08 15:49:10 -0400

    added dynamic tagging awesome wm

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -461,5 +461,9 @@ return require('packer').startup(function()
     run = function() vim.fn['firenvim#install'](0) end
   }
 
-  use {'Xuyuanp/scrollbar.nvim', disable = true}
+  use {
+    'Xuyuanp/scrollbar.nvim',
+    disable = true,
+    config = function() require'config/scrollbar'.setup() end,
+  }
 end)

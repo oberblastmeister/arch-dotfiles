c6e012c7488376ee3cb99f1b1364ffece3005d99
Brian Shu <littlebubu.shu@gmail.com>
2020-10-16 19:00:19 -0400

    changed bindings in tree

--- .config/nvim/lua/config/tree.lua
+++ .config/nvim/lua/config/tree.lua
@@ -2,11 +2,13 @@ local function setup()
   vim.g.lua_tree_ignore = {".git"}
   vim.g.lua_tree_auto_close = 1
   vim.g.lua_tree_follow = 1
+
   vim.g.lua_tree_show_icons = {
     git = 1,
     folders = 1,
     files = 1
   }
+
   vim.g.lua_tree_icons = {
     default = "",
     git = {
@@ -20,6 +22,26 @@ local function setup()
       default = "",
       open = ""
     }
+  }
+
+  vim.g.lua_tree_bindings = {
+    edit = {'<CR>', 'o'},
+    edit_vsplit = '<C-v>',
+    edit_split = '<C-x>',
+    edit_tab = '<C-t>',
+    toggle_ignored = '.',
+    toggle_dotfiles = 'H',
+    refresh = 'R',
+    preview = '<Tab>',
+    cd = 'l',
+    create = 'a',
+    remove = 'D',
+    rename = 'r',
+    cut = 'd',
+    copy = 'y',
+    paste = 'p',
+    prev_git_item = '[c',
+    next_git_item = ']c',
   }
 
   vim.api.nvim_exec(

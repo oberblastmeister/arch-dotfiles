d2dd0a75f8c2fc1be97f6a8b904c21628f0c0098
Brian Shu <littlebubu.shu@gmail.com>
2020-10-18 15:18:46 -0400

    added markdown config to markdown.lua

--- .config/nvim/lua/filetypes/markdown.lua
+++ .config/nvim/lua/filetypes/markdown.lua
@@ -2,6 +2,15 @@ local M = {}
 local loop = vim.loop
 local api = vim.api
 
+function M.setup()
+  vim.g.vim_markdown_no_default_key_mappings = 1
+  vim.g.vim_markdown_conceal = 0
+  vim.g.vim_markdown_conceal_code_blocks = 0
+  vim.g.vim_markdown_auto_insert_bullets = 1
+  vim.g.vim_markdown_math = 1
+  vim.g.vim_markdown_strikethrough = 1
+end
+
 function M.convert_file()
   local shortname = vim.fn.expand('%:t:r')
   local fullname = api.nvim_buf_get_name(0)

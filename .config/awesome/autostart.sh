7418e7376cc244fff6a862eeb878ab959f3c0a60
Brian Shu <littlebubu.shu@gmail.com>
2020-11-24 12:52:16 -0500

    added autostart

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -52,6 +52,7 @@ run redshift-gtk
 run syncthing-gtk -m
 run greenclip daemon
 run mailspring --background
+run unclutter --timeout 4
 ~/bin/pulse-volume-watcher.py | xob -s gruvbox &
 configs &
 setupxob &
